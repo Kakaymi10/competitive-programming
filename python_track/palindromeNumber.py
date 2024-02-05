f22fc838348937a9289c20d807a6ca7093ddc16c
Kakaymi1o <m.moussa@alustudent.con>
2024-02-05 21:00:17 +0200

    CP

---
+++ python_track/palindromeNumber.py
@@ -0,0 +1,44 @@
+'''
+9. Palindrome Number
+Solved
+Easy
+Topics
+Companies
+Hint
+Given an integer x, return true if x is a
+palindrome
+, and false otherwise.
+
+
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+
+Constraints:
+
+-231 <= x <= 231 - 1
+'''
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        l, r = 0, len(str(x)) - 1
+        while l <= r:
+            if str(x)[l] != str(x)[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
